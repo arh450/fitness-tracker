514eceab61671fda5604e8a9b83c8f699376db46
hank amadeus <hankamadeus@email.com>
2020-07-21 10:18:45 -0400

    routed and set up server connection

---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+// Add exercises to a previous workout plan. (PUT)
+
+
+// Add new exercises to a new workout plan (POST)
+
+
+// View multiple the combined weight of multiple exercises on the stats page. (GET)
+
+
+const router = require('express').Router();
+const db = require('../models');
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// middleware for morgan and express
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// routes
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout';
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
