b1e02b1665bbcd0b08ab8527f22eea85bbf55a3c
hank amadeus <hankamadeus@email.com>
2020-07-24 10:09:34 -0400

    deployed to heroku

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 // middleware for morgan and express
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // routes
-app.use('/', htmlRoutes);
-app.use('/api', apiRoutes);
+app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
 
-let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout';
+let MONGODB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb://user:1password@ds145380.mlab.com:45380/heroku_v3ptjj2x";
 
 mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useFindAndModify: false,
 });
 
-
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
